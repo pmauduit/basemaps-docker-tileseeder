81135220b1dde6c19c3cf0db13518f789d25d5b0
Pierre Maduit <pierre.mauduit@gmail.com>
2016-04-02 19:23:53 +0200

    imposm - reordering bootstrap
    - First fetches the osm extract (which should let sufficient amount of time to
      be sure the database is fully set up)
    - Then check the existence of the template_postgis before trying to create the
      osm db

--- imposm/bootstrap.sh
+++ imposm/bootstrap.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-until `nc -z database 5432`; do
-  echo "$(date) - waiting for postgres (localhost-only)..."
-  sleep 20
+mkdir -p /tmp/imposm-bootstrap
+cd /tmp/imposm-bootstrap
+
+if [ -z "${CUSTOM_PBF_EXTRACT_URL}" ] ; then
+  wget --no-verbose -O extract.pbf http://download.geofabrik.de/europe/france/rhone-alpes-latest.osm.pbf
+else
+  wget --no-verbose -O extract.pbf ${CUSTOM_PBF_EXTRACT_URL} ;
+fi
+
+PGPASSWORD=osm psql -h database -U osm -l | grep template_postgis
+while [ $? -ne 0 ]; do
+  echo "$(date) - waiting for postgres ..."
+  sleep 60
+  PGPASSWORD=osm psql -h database -U osm -l | grep template_postgis
 done
 echo "creating db osm"
 echo "CREATE DATABASE osm ENCODING 'UTF8' TEMPLATE template_postgis;" | PGPASSWORD=osm psql -h database -U osm -d postgres
 echo "adding postgis extension to db osm"
 echo "CREATE EXTENSION postgis;" | PGPASSWORD=osm psql -h database -U osm -d osm
 
-mkdir -p /tmp/imposm-bootstrap
-cd /tmp/imposm-bootstrap
-
-if [ -z "${CUSTOM_PBF_EXTRACT_URL}" ] ; then
-  wget -O extract.pbf http://download.geofabrik.de/europe/france/rhone-alpes-latest.osm.pbf
-else
-  wget -O extract.pbf ${CUSTOM_PBF_EXTRACT_URL} ;
-fi
 
 imposm --read --write --overwrite-cache --optimize --mapping-file /imposm-mapping.py --connection postgis://osm:osm@database/osm extract.pbf
